3c0473e78c2f84d498738f8012fde770c07d50a1
MukukaCh <chewe.mukuka@gmail.com>
2020-01-16 09:51:52 +0200

    Feature: Updated test Variable name and path to read from

--- Writecsv/csvTests/WritecvsfilesTests.cs
+++ Writecsv/csvTests/WritecvsfilesTests.cs
@@ -11,46 +11,40 @@ using System.Linq;
 namespace Tests
 {
     [TestClass()]
-    public class WritecvsfilesTests
+    public class WritecvsfilesTests 
     {
         [TestMethod()]
         public void WritecvsTest()
         {
-            var path = "c://csvfiles//worldcities.csv";
+            var path = "c://Users//mukukach//Documents//csvfiles//worldcities.csv";
             var doubleTypeConversion = new DoubleConversion();
             IList<CityModel> myList = ReadCsv.ReadCsvFile<CityModel, CityMap>(path, doubleTypeConversion);
-            var variablename = from s in myList
+            var Uscountry = from s in myList
                                       where s.Country.Equals("United States")
                                       orderby s.Country ascending
                                       select s;
             // SOme Update
-            foreach (CityModel city in variablename)
+            foreach (CityModel city in Uscountry)
             {
                 Debug.Write(city.Country + ": " + city.City_name + Environment.NewLine);
             }
-            var queryName = nameof(variablename);
+            var queryName = nameof(Uscountry);
             var writePath = "c://csvfiles//" + queryName + ".csv";
             using (var writer = new StreamWriter(writePath))
             using (var csv = new CsvWriter(writer))
             {
-                csv.WriteRecords(variablename);
+                csv.WriteRecords(Uscountry);
             }
             Assert.IsTrue(File.Exists(writePath));
 
-            var QSCount = (from city in variablename
+            var QSCount = (from city in Uscountry
                            select city).Count();
 
             Debug.Write(QSCount);
 
             Assert.AreEqual(15493, myList.Count());
 
-            // countryQuery = records.Where(city => city.Country.Equals("United States"));
-            /*
-            foreach (CityModel city in countryQuery)
-            {
-                var name = city.City_name.ToString();
-            }
-            */
+           
         }
     }
 }
